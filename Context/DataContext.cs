3972dd22df64943dd17d6f62bf9af2de97ad8ae7
Natanael Augustin <natanael.augustin@gmail.com>
2023-10-05 11:00:48 +0200

    added a context

---
+++ Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using umbraco_course_submission.Entities;
+
+namespace umbraco_course_submission.Context
+{
+    public class DataContext : DbContext
+    {
+
+        protected DataContext()
+        {
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<SubsEntity> Subs { get; set; }
+    }
+}
