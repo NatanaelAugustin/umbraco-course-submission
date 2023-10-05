5016eb052d39879dfb38dc02dfcf9717f7e5c3e6
Natanael Augustin <natanael.augustin@gmail.com>
2023-10-05 11:01:19 +0200

    added a newsletter submission section

---
+++ Entities/SubsEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace umbraco_course_submission.Entities
+{
+    public class SubsEntity
+    {
+        [Key]
+        public string Email { get; set; } = null!;
+    }
+}
