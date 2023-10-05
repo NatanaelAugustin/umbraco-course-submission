d929403d274d64c4e09865897d2ec592624c7bcc
Natanael Augustin <natanael.augustin@gmail.com>
2023-10-05 11:37:30 +0200

    updated subsService

---
+++ Service/SubsService.cs
@@ -0,0 +1,23 @@
+﻿using umbraco_course_submission.Context;
+using umbraco_course_submission.Models;
+
+namespace umbraco_course_submission.Service
+{
+    public class SubService
+    {
+
+        private readonly DataContext _context;
+
+        public SubService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSubsAsync(NewsLetterForm form)
+        {
+            _context.Subs.Add(new Entities.SubsEntity { Email = form.Email });
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
